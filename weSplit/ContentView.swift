d18cbe06854b5b16b8d2a747ccb68336e6dad92a
Nurşah Ari <nursah.ari@partner.turktelekom.com.tr>
2022-08-20 21:17:03 +0300

    Initial Commit

---
+++ weSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  weSplit
+//
+//  Created by Nurşah ARİ on 20.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
